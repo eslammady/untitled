6aaf2cb0066b0bd46123bdf508615eb8e003ba5b
admin <eslamabomady2017@gmail.coom>
2024-03-02 15:11:25 +0200

    new application

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/Shared/Screens/LoginScreen.dart';
+import 'package:untitled/Shared/Screens/Login_Screen2.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: loginScreen(),
+    );
+  }
+}
